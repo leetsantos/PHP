845f7034fe975a3b7fdcf9a470628a1b5b597709
email <leticia.silva@webjump.com.br>
2022-01-06 16:20:13 -0300

    Atualizações

--- imc.php
+++ imc.php
@@ -4,21 +4,21 @@ $altura= readline("Digite sua altura:  ");
 $imc= $peso/(($altura)**2);
 
 if ($imc<18.5) {
-    echo "Abaixo do peso" . PHP_EOL . "IMC:$imc";
+    echo "Abaixo do peso" . PHP_EOL ;
     }
     elseif ($imc<25) {
-        echo "Peso Normal" . PHP_EOL . "IMC: $imc";
+        echo "Peso Normal" . PHP_EOL;
     }
         elseif ($imc<30) {
-            echo "Sobrepeso" . PHP_EOL . "IMC: $imc";
+            echo "Sobrepeso" . PHP_EOL;
         }
             elseif($imc<35){
-                echo "Obesidade nível 1". PHP_EOL . "IMC: $imc";
+                echo "Obesidade nível 1". PHP_EOL;
             }
-                elseif ($imc<40){
-                    echo "Obesidade Nível 2". PHP_EOL . "IMC: $imc";
+                elseif ($imc<40) {
+                    echo "Obesidade Nível 2" . PHP_EOL;
                 }
                     else {
-                        echo "Obesidade Morbida" . PHP_EOL . "IMC $imc";
+                        echo "Obesidade Morbida" . PHP_EOL;
                     }
-
+echo "IMC:$imc" . PHP_EOL;
